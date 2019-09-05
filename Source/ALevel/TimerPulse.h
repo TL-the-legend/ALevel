13daaa24e70e062c9cb4fbab4820ba0d55555949
TL-the-legend <TL-the-legend>
2019-09-05 22:23:18 +0100

    Timer1.1 Delegate2.1

---
+++ Source/ALevel/TimerPulse.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/Engine.h"
+#include "Runtime/Engine/Public/TimerManager.h"
+#include "TimerPulse.generated.h"
+
+// Part of delegate
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSendPulse, bool, Pulse);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ALEVEL_API UTimerPulse : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTimerPulse();
+
+	// Part of delegate
+	UPROPERTY(BlueprintAssignable)
+	FSendPulse TickPulse;
+
+	// Timer
+	void TimerLoop();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
