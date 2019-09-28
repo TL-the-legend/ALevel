6c9eaa984df263d42e49798ce0d0261f5bf55aac
TL-the-legend <TL-the-legend>
2019-09-28 23:47:49 +0100

    commit for removing uncommited stuff

---
+++ Source/ALevel/Cell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CellGeneratorAndController.h"
+#include "Cell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALEVEL_API ACell : public ACellGeneratorAndController
+{
+	GENERATED_BODY()
+	
+};
